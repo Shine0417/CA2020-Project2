0d874762caad2016a1f41ecb3b19b0944e05464e
Shine0417 <b07902109@ntu.edu.tw>
2021-01-04 23:43:55 +0800

    fix comment

--- codes/dcache_sram.v
+++ codes/dcache_sram.v
@@ -51,12 +51,12 @@ always@(posedge clk_i or posedge rst_i) begin
     hit_id = (tag[addr_i][0][24] && (tag[addr_i][0][22:0] == tag_i[22:0]))? 0 : (tag[addr_i][1][24] && (tag[addr_i][1][22:0] == tag_i[22:0]))? 1 : 2;
     if (enable_i && write_i) begin
         // TODO: Handle your write of 2-way associative cache + LRU here
-        if (hit_id != 2) begin // Write hit -> dirty bit = 1
+        if (hit_id != 2) begin // Write hit
             tag [addr_i][hit_id] <= tag_i;
             data[addr_i][hit_id] <= data_i;
             LRU [addr_i] <= hit_id;
         end 
-        else begin // Read miss -> dirty bit = 0
+        else begin // Read miss
             tag [addr_i][LRU[addr_i]] <= tag_i;
             data[addr_i][LRU[addr_i]] <= data_i;
             LRU [addr_i] <= ~LRU[addr_i];
